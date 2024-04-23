4078ddfae6f9d93072f79bff13eb137223ad9b70
Alan Henrique <alanhipermassa@gmail.com>
2024-04-23 19:47:53 -0300

    Created Service class to Cliente

---
+++ PDV/src/main/java/br/unipar/programacaointernet/pdv/service/ClienteService.java
@@ -0,0 +1,30 @@
+package br.unipar.programacaointernet.pdv.service;
+
+import br.unipar.programacaointernet.pdv.objetos.Cliente;
+import br.unipar.programacaointernet.pdv.repository.ClienteRepository;
+import jakarta.inject.Inject;
+
+import java.util.List;
+
+public class ClienteService {
+
+    @Inject
+    private ClienteRepository repository;
+
+    public List<Cliente> listar() {
+        return repository.getAll();
+    }
+
+    public Cliente listarById(Integer id) {
+        return repository.getById(id);
+    }
+
+    public void cadastrar(Cliente cliente) {
+        repository.insert(cliente);
+    }
+
+    public void deletar(Cliente cliente) {
+        repository.remove(cliente);
+    }
+}
+
