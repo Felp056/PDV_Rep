80352c8c13d038b58396883f69c35b4876e089c6
Alan Henrique <alanhipermassa@gmail.com>
2024-04-23 21:25:14 -0300

    Import ProdutoService

--- PDV/src/main/java/br/unipar/programacaointernet/pdv/controller/ProdutoController.java
+++ PDV/src/main/java/br/unipar/programacaointernet/pdv/controller/ProdutoController.java
@@ -2,6 +2,7 @@ package br.unipar.programacaointernet.pdv.controller;
 
 import br.unipar.programacaointernet.pdv.objetos.Produto;
 import br.unipar.programacaointernet.pdv.objetos.Venda;
+import br.unipar.programacaointernet.pdv.service.ProdutoService;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
