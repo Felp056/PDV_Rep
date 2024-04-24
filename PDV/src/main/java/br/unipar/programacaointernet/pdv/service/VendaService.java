fe96c6e3f91f14a660b78449eff4a742678737ca
Alan Henrique <alanhipermassa@gmail.com>
2024-04-23 21:27:20 -0300

    Created Service class to Venda

---
+++ PDV/src/main/java/br/unipar/programacaointernet/pdv/service/VendaService.java
@@ -0,0 +1,29 @@
+package br.unipar.programacaointernet.pdv.service;
+
+import br.unipar.programacaointernet.pdv.objetos.Venda;
+import br.unipar.programacaointernet.pdv.repository.VendaRepository;
+import jakarta.inject.Inject;
+
+import java.util.List;
+
+public class VendaService {
+
+    @Inject
+    private VendaRepository repository;
+
+    public List<Venda> listar() {
+        return repository.getAll();
+    }
+
+    public Venda listarById(Integer id) {
+        return repository.getById(id);
+    }
+
+    public void cadastrar(Venda venda) {
+        repository.insert(venda);
+    }
+
+    public void deletar(Venda venda) {
+        repository.remove(venda);
+    }
+}
