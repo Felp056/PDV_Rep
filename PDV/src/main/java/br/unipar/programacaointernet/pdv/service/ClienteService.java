4ad66d66b83b82698a7164e790f5985c8dfb16ed
Alan Henrique <alanhipermassa@gmail.com>
2024-05-06 22:40:24 -0300

    Listar ClienteTelefoneAniversario

--- PDV/src/main/java/br/unipar/programacaointernet/pdv/service/ClienteService.java
+++ PDV/src/main/java/br/unipar/programacaointernet/pdv/service/ClienteService.java
@@ -1,7 +1,10 @@
 package br.unipar.programacaointernet.pdv.service;
 
+import br.unipar.programacaointernet.pdv.dto.ClienteTelefoneNascimentoDTO;
+import br.unipar.programacaointernet.pdv.mapper.ClienteMapper;
 import br.unipar.programacaointernet.pdv.objetos.Cliente;
 import br.unipar.programacaointernet.pdv.repository.ClienteRepository;
+import br.unipar.programacaointernet.pdv.repository.VendaRepository;
 import jakarta.ejb.Stateless;
 import jakarta.inject.Inject;
 
@@ -11,6 +14,7 @@ public class ClienteService {
 
     @Inject
     private ClienteRepository repository;
+    private VendaRepository vendaRepository;
 
     public List<Cliente> listar() {
         return repository.getAll();
@@ -20,12 +24,21 @@ public class ClienteService {
         return repository.getById(id);
     }
 
+    public List<ClienteTelefoneNascimentoDTO> listarClienteTelefoneAniversario() {
+        List<Cliente> clienteList = repository.getAll();
+        return ClienteMapper.toDTO(clienteList);
+    }
+
     public void cadastrar(Cliente cliente) {
         repository.insert(cliente);
     }
 
     public void deletar(Cliente cliente) {
         repository.remove(cliente);
+    }
+
+    public void atualizar(Cliente cliente) {
+        repository.update(cliente);
     }
 }
 
